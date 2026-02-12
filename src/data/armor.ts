699a0dc7b386a7da69554557c18c09234426a4aa
Steve Woody <stevewoody82@gmail.com>
2026-02-12 13:04:49 +0000

    Citadel-SE: remove White from title; Morozov Default -> Horizon

--- src/data/armor.ts
+++ src/data/armor.ts
@@ -71,7 +71,7 @@ export const armors: ArmorItem[] = [
       { slot: 'Legs', item: 'Morozov-SH Legs' },
       { slot: 'Backpack', item: 'Morozov-CH Backpack Pyrotechnic' },
     ],
-    variants: ['Pyrotechnic', 'Default', 'Aftershock', 'Redshift', 'Snowdrift', 'Terracotta', 'Crusader', 'Red Alert'],
+    variants: ['Pyrotechnic', 'Horizon', 'Aftershock', 'Redshift', 'Snowdrift', 'Terracotta', 'Crusader', 'Red Alert'],
     image: 'armor-images/morozov.png',
     rare: true,
   },
@@ -150,7 +150,7 @@ export const armors: ArmorItem[] = [
   },
   {
     id: 'citadel-se-white',
-    name: 'Citadel-SE White',
+    name: 'Citadel-SE',
     type: 'Heavy',
     manufacturer: 'Kastak Arms',
     where: 'Executive Hangars / high-tier loot areas',
